c1e773922e6f31893841fa1aba5b3f7778ccacfd
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-11-22 18:13:00 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  switch(true){
+    case value <= 400:
+      return 'This one is on me!';
+    case value > 2000 && value <= 2500:
+      return 'I will gladly take your thirty bucks.'
+      case value > 2500:
+        return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(value){
   // Write your code here!
+ return value === 'NYC'?'Ok, sounds good.':'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+  switch(true){
+    case value === 'generous':
+      return 'Thank you so much.';
+    case value === 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.';
+  }
 }
